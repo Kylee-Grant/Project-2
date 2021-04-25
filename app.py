856ce74c376185f85c4ed7f33308d501b3839aa0
Kylee-Grant <72223513+Kylee-Grant@users.noreply.github.com>
2021-04-25 00:43:55 -0500

    updating with Heroku files, prepping to deploy app

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 # Based off of model from activity 10.3.10
 import numpy as np
 
-import sqlalchemy
+#import sqlite3
+#import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-import sqlite3
 
 from flask import Flask, render_template, redirect, jsonify, json, request
 
 #################################################
-# Database Setup: 
+# Database Setup (examples: https://pythonbasics.org/flask-sqlite/, https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/)
+# This approach is based off of model from activity 10.3.10.
 #################################################
 engine = create_engine("sqlite:///static/data/birthdata.sqlite")
 
@@ -39,44 +40,25 @@ def line_chart():
     session = Session(engine)
 
     """Return a list of all state names"""
-    # Query all passengers
-    results = session.query(National).all()
+    # Query all 
+    resultsUS = session.query(National.year, National.us_birth_rate).order_by(National.year.asc()).distinct()
+    resultsState = session.query(National.state, National.year, National.state_rate).order_by(National.year.asc()).all()
+    # Use of distinct: https://stackoverflow.com/questions/48102501/remove-duplicates-from-sqlalchemy-query-using-set
 
+    USData = []
+    for r in resultsUS: 
+        USData.append({"rate": r[1], "year": r[0]})
+    
+    stateData = []
+    for r in resultsState:
+        stateData.append({"rate": r[2], "state": r[0], "year": r[1]})
+    
     session.close()
 
     # Convert list of tuples into normal list
-    #all = list(np.ravel(results))
+    #data = list(np.ravel(results))
+    return render_template("line_chart.html", USData=USData, stateData=stateData)
 
-    return render_template("line_chart.html", data=results)
-
-
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# Comment this out when not in development
+#if __name__ == '__main__':
+   #app.run(debug=True)
--- static/js/line_Chart_LH copy.js
+++ static/js/line_Chart_LH copy.js
@@ -20,7 +20,7 @@ function makeLine (state, colorIndex) {
   return line;
 }
 
-// sample data entry to be replaced by api call to flask app
+  // sample data entry to be replaced by api call to flask app
 var US = [41.1, 40.5, 39.7, 41.1, 41.5, 40.2, 37.9, 34.2, 31.3, 29.4, 26.6, 24.2, 22.3, 20.3, 18.8, 17.4];
 
 var stateData = {
@@ -28,6 +28,8 @@ IL: [40.2, 40.1, 38.5, 39.8, 40.2, 38.5, 35.9, 33, 29.5, 27.9, 24.6, 22.8, 21.1,
 AL: [51.4, 51, 48.1, 51.8, 52.1, 50.5, 48.3, 43.6, 40.5, 39.2, 34.3, 32, 30.1, 28.4, 27, 25.2],
 NH: [18, 18.1, 18, 18.1, 19.3, 19.1, 16.4, 15.7, 13.7, 13.8, 12.6, 11, 10.9, 9.3, 8.4, 8]
 };
+
+
 
 // chart set up with just US totals
 
@@ -43,7 +45,7 @@ var data = {
    datasets: datasets
 };
 
-// chart config
+  // chart config
 
 const config = {
   type: 'line',
@@ -106,6 +108,7 @@ const config = {
 
 
 d3.select("#add_state").on("click", function(){
+
   var selection = d3.select("#state_input").property("value");
   console.log(selection);
   newLine = makeLine(selection, 2);
